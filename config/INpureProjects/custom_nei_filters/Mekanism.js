4929d70ce35b0eb75b0e4320eaeb91c5ef93595a
BBoldt <bboldt92@yahoo.com>
2015-03-30 14:48:19 -0700

    Mek / Inpurecore changes
    * nerfed wind turbine a bit again
    * hid tanks from NEI using inpurecore

--- config/INpureProjects/custom_nei_filters/Mekanism.js
+++ config/INpureProjects/custom_nei_filters/Mekanism.js
@@ -3,4 +3,6 @@ if (FML.isModLoaded("Mekanism")) {
     NEI.override("Mekanism:*PlasticBlock", [0]);
     NEI.override("Mekanism:Balloon", [0]);
     NEI.override("Mekanism:GlowPanel", [0]);
+    NEI.override("Mekanism:PlasticFence", [0]);
+    NEI.override("Mekanism:MachineBlock2", [11]);
 }
